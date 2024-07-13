ed0889664502d2a335e133a9ce4709673ad6d5e2
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-13 18:04:24 +0400

    Initial commit

---
+++ src/main/java/com/cydeo/TicketingProjectRestV2Application.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingProjectRestV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingProjectRestV2Application.class, args);
+    }
+
+}
