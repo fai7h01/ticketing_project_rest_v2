216fa31a517e452753dba3fccbb2fd04e8499cda
fai7h01 <l.varsimashvil10@gmail.com>
2024-07-13 20:28:13 +0400

    Task Controller

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -1,13 +1,11 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.TaskDTO;
 import com.cydeo.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v2/task")
@@ -35,6 +33,33 @@ public class TaskController {
                 .code(HttpStatus.OK.value())
                 .message("Task is successfully retrieved.")
                 .data(taskService.findById(id)).build());
+    }
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createTask(@RequestBody TaskDTO taskDTO){
+        taskService.save(taskDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(ResponseWrapper.builder()
+                .success(true)
+                .code(HttpStatus.CREATED.value())
+                .message("Task is successfully created.").build());
+    }
+
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateTask(@RequestBody TaskDTO taskDTO){
+        taskService.update(taskDTO);
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .success(true)
+                .code(HttpStatus.OK.value())
+                .message("Task is successfully updated.").build());
+    }
+
+    @DeleteMapping("/{taskId}")
+    public ResponseEntity<ResponseWrapper> deleteTask(@PathVariable("taskId") Long id){
+        taskService.delete(id);
+        return ResponseEntity.ok(ResponseWrapper.builder()
+                .success(true)
+                .code(HttpStatus.OK.value())
+                .message("Task is successfully deleted.").build());
     }
 
 }
